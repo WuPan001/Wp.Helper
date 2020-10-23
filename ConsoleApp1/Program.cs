68144fe58e4c1b9e0140e3ed604b1ca34ffe3844
WuPan001 <37973279+WuPan001@users.noreply.github.com>
2020-10-23 15:47:04 +0800

    新增Math帮助类

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,22 +14,38 @@ namespace ConsoleApp1
         {
             try
             {
-                foreach (var item in FileHelper.GetFilesFullName(true))
-                {
-                    Console.WriteLine(item);
-                }
-                foreach (var item in FileHelper.GetFilesFullName())
-                {
-                    Console.WriteLine(item);
-                }
-                foreach (var item in FileHelper.GetDirectoriesFullName())
-                {
-                    Console.WriteLine(item);
-                }
-                foreach (var item in FileHelper.GetFileSystemInfo())
-                {
-                    Console.WriteLine(item);
-                }
+                #region 文件帮助类测试
+
+                //foreach (var item in FileHelper.GetFilesFullName(true))
+                //{
+                //    Console.WriteLine(item);
+                //}
+                //foreach (var item in FileHelper.GetFilesFullName())
+                //{
+                //    Console.WriteLine(item);
+                //}
+                //foreach (var item in FileHelper.GetDirectoriesFullName())
+                //{
+                //    Console.WriteLine(item);
+                //}
+                //foreach (var item in FileHelper.GetFileSystemInfo())
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+                #endregion 文件帮助类测试
+
+                #region math帮助类测试
+
+                //Console.WriteLine(MathHelper.ToChinese(Convert.ToDecimal(123.45)));
+
+                #endregion math帮助类测试
+
+                #region
+
+                //
+
+                #endregion
             }
             catch (Exception ex)
             {
---
+++ Wp.Helpers/MathHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Wp.Helpers
+{
+    public class MathHelper
+    {
+        /// <summary>
+        /// 将金额转成中文大写
+        /// </summary>
+        /// <returns></returns>
+        public static string ToChinese(decimal amount)
+        {
+            var s = amount.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A");
+            var d = Regex.Replace(s, @"((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\.]|$))))", "${b}${z}");
+            return Regex.Replace(d, ".", m => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟万亿兆京垓秭穰"[m.Value[0] - '-'].ToString());
+        }
+    }
+}
