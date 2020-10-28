fe46022dc6755beb3dcda3b49986788b2f0023b2
WuPan001 <37973279+WuPan001@users.noreply.github.com>
2020-10-28 09:29:03 +0800

    完善svg帮助类

--- Wp.Helpers/SvgHelper.cs
+++ Wp.Helpers/SvgHelper.cs
@@ -16,17 +16,18 @@ namespace Wp.Helpers
         /// 获取图片图案几何路径
         /// </summary>
         /// <param name="filePath">图片文件路径</param>
+        /// <param name="splits">拆分字符串集合，默认为null后，集合为 new string[] { "path d=\"", "\" fill=\"", "\" p-id=\"" }</param>
         /// <returns></returns>
-        public static string GetGeometryPath(string filePath)
+        public static string GetGeometryPath(string filePath, string[] splits = null)
         {
             var res = new StringBuilder();
             if (File.Exists(filePath))
             {
                 var text = File.ReadAllText(filePath);
-                var temp = text.Split(new string[] { "path d=\"", "\" fill=\"" }, 1024, StringSplitOptions.RemoveEmptyEntries);
-                for (int i = 0; i < temp.Length; i++)
+                var temp = text.Split(splits == null ? new string[] { "path d=\"", "\" fill=\"", "\" p-id=\"" } : splits, 1024, StringSplitOptions.RemoveEmptyEntries);
+                for (byte i = 0; i < temp.Length; i++)
                 {
-                    if (i % 2 != 0)
+                    if (temp[i][0] == 'M' && temp[i][temp[i].Length - 1] == 'z')
                     {
                         res.Append($"{temp[i]} ");
                     }
@@ -44,8 +45,11 @@ namespace Wp.Helpers
         /// 获取图片图案几何路径样式
         /// </summary>
         /// <param name="fileInfo">图片文件路径</param>
+        /// <param name="brush">图案其他状态时的颜色，{StaticResource MainSolidColorBurshStyle} or #e0620d</param>
+        /// <param name="normalBrush">图案默认颜色，{StaticResource TertiaryTextSolidColorBurshStyle} or #707070</param>
         /// <returns></returns>
-        public static Dictionary<string, string> GetGeometryStyle(Dictionary<string, string> fileInfo)
+        public static Dictionary<string, string> GetGeometryStyle(Dictionary<string, string> fileInfo,
+            string brush = "{StaticResource MainSolidColorBurshStyle}", string normalBrush = "{StaticResource TertiaryTextSolidColorBurshStyle}")
         {
             var res = new Dictionary<string, string>();
             var content = new StringBuilder();
@@ -56,7 +60,20 @@ namespace Wp.Helpers
                 content.AppendLine($"<DrawingImage x:Key=\"{item}\">");
                 content.AppendLine("<DrawingImage.Drawing>");
                 content.AppendLine("<DrawingGroup>");
-                content.Append("<GeometryDrawing Brush = \"{StaticResource MainSolidColorBurshStyle}\" Geometry=\"{StaticResource ");
+                content.Append("<GeometryDrawing Brush=\"");
+                content.Append($"{brush}");
+                content.Append("\" Geometry=\"{StaticResource ");
+                content.Append($"{item}Geometry");
+                content.AppendLine("}\"/>");
+                content.AppendLine("	</DrawingGroup>");
+                content.AppendLine("</DrawingImage.Drawing>");
+                content.AppendLine("</DrawingImage>");
+                content.AppendLine($"<DrawingImage x:Key=\"{item}Normal\">");
+                content.AppendLine("<DrawingImage.Drawing>");
+                content.AppendLine("<DrawingGroup>");
+                content.Append("<GeometryDrawing Brush=\"");
+                content.Append($"{normalBrush}");
+                content.Append("\" Geometry=\"{StaticResource ");
                 content.Append($"{item}Geometry");
                 content.AppendLine("}\"/>");
                 content.AppendLine("	</DrawingGroup>");
