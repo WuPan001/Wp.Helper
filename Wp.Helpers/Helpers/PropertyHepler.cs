80fbacdc865ea3799550e327be16e96924ab46ea
WuPan <wu_pan123@126.com>
2021-07-10 16:45:01 +0800

    完善对象属性帮助类

--- Wp.Helpers/Helpers/PropertyHepler.cs
+++ Wp.Helpers/Helpers/PropertyHepler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace Wp.Helpers.Helpers
 {
@@ -32,28 +33,28 @@ namespace Wp.Helpers.Helpers
         }
 
         /// <summary>
-        /// 获取类属性描述和属性值
+        /// 获取类属性描述和属性名
         /// 属性描述为key
         /// </summary>
         /// <typeparam name="T">类类型</typeparam>
         /// <param name="obj">类实例</param>
-        /// <param name="ExceptType">不需返回的属性类型</param>
+        /// <param name="exceptType">不需返回的属性类型</param>
         /// <param name="bindingFlags">属性类型</param>
         /// <returns></returns>
         public static Dictionary<string, string> GetPropertyDescriptionKeyIsDescription<T>(
             T obj,
-            List<Type> ExceptType = null,
+            List<Type> exceptType = null,
             BindingFlags bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance) where T : class
         {
             try
             {
                 var res = new Dictionary<string, string>();
                 PropertyValue<T> propertyValue = new PropertyValue<T>(obj);
-                if (ExceptType != null)
+                if (exceptType != null)
                 {
                     foreach (PropertyInfo p in typeof(T).GetProperties(bindingFlags))
                     {
-                        if (ExceptType.Contains(p.PropertyType))
+                        if (exceptType.Contains(p.PropertyType))
                         {
                             //
                         }
@@ -82,28 +83,28 @@ namespace Wp.Helpers.Helpers
         }
 
         /// <summary>
-        /// 获取类属性描述和属性值
+        /// 获取类属性名和属性值
         /// 属性名为key
         /// </summary>
         /// <typeparam name="T">类类型</typeparam>
         /// <param name="obj">类实例</param>
-        /// <param name="ExceptType">不需返回的属性类型</param>
+        /// <param name="exceptType">不需返回的属性类型</param>
         /// <param name="bindingFlags">属性类型</param>
         /// <returns></returns>
         public static Dictionary<string, string> GetPropertyValueKeyIsName<T>(
             T obj,
-            List<Type> ExceptType = null,
+            List<Type> exceptType = null,
             BindingFlags bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance) where T : class
         {
             try
             {
                 PropertyValue<T> propertyValue = new PropertyValue<T>(obj);
                 var res = new Dictionary<string, string>();
-                if (ExceptType != null)
+                if (exceptType != null)
                 {
                     foreach (PropertyInfo p in typeof(T).GetProperties(bindingFlags))
                     {
-                        if (ExceptType.Contains(p.PropertyType))
+                        if (exceptType.Contains(p.PropertyType))
                         {
                             //
                         }
@@ -128,6 +129,45 @@ namespace Wp.Helpers.Helpers
             {
                 throw;
             }
+        }
+
+        /// <summary>
+        /// 对象转字符串
+        /// 将对象属性值输出，格式为属性名：属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="exceptType"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
+        public static string ObjectToStringByNameValue<T>(
+            T obj,
+            List<Type> exceptType = null,
+            BindingFlags bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance) where T : class
+        {
+            var dic = GetPropertyValueKeyIsName(obj, exceptType, bindingFlags);
+            var res = new StringBuilder();
+            foreach (var item in dic.Keys)
+            {
+                res.Append($"{item}:{dic[item]}  ");
+            }
+            return res.ToString();
+        }
+
+        /// <summary>
+        /// 对象转字符串
+        /// 将对象属性值输出，格式为属性描述：属性值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="ExceptType"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
+        public static string ObjectToStringByDescriptionValue<T>(T obj,
+            List<Type> ExceptType = null,
+            BindingFlags bindingFlags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance)
+        {
+            return null;
         }
 
         public class PropertyValue<T>
